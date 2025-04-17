d9689019036bb1569dedb0989d5a00698d1aec28
dmdthd2 <dmdthd2@g.yju.ac.kr>
2025-04-17 11:06:59 +0900

    강영준의 w

--- eclipse-workspace/eclipse-workspace/chap01/src/Hello.java
+++ eclipse-workspace/eclipse-workspace/chap01/src/Hello.java
@@ -1,5 +1,5 @@
 
-public class Hellow {
+public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hellow java!!");
--- eclipse-workspace/eclipse-workspace/chap01/src/Hello2.java
+++ eclipse-workspace/eclipse-workspace/chap01/src/Hello2.java
@@ -1,5 +1,5 @@
 
-public class Hellow2 {
+public class Hello2 {
 
 	public static void main(String[] args) {
 		System.out.println("내가 만든 자바 프로그램");
